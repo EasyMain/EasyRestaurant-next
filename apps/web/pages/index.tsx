97a599dfd9a2ddc03f2331d37bf2f43a9cf4080d
Carlos Garrido Marín <48090366+cgm164@users.noreply.github.com>
2022-04-19 23:38:20 +0200

    Testing example pages for UI

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,14 +1,26 @@
-import { Button } from "ui";
+import { InputBase } from "ui";
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button onPress={() => console.log('is clicked')}
+    <div className="flex justify-center py-20 px-10">
+      {/* <Button onPress={() => console.log('is clicked')}
         type="reset"
       >
         Prueba
-      </Button>
+      </Button> */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Submit");
+        }}
+        className="w-full flex flex-col gap-10"
+      >
+        <InputBase
+          label="Descripción"
+          description="Esto es una descripcion"
+        />
+        <button type="submit">Aqui</button>
+      </form>
     </div>
   );
 }
